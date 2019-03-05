97266db002ade07fa37e17cb40fdb24bae5a67f1
Matt Nakama <matt+git@mattnakama.com>
2019-03-05 17:03:46 -0500

    improve emacs startup performance

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,16 @@
+; -*- lexical-binding: t; -*-
+
+; startup performance enhancements
+(setq gc-cons-threshold (eval-when-compile (* 256 1024 1024)))
+
+(let ((saved-alist file-name-handler-alist))
+  (defun post-init-restore ()
+	(setq gc-cons-threshold (eval-when-compile (* 16 1024 1024))
+		  file-name-handler-alist saved-alist))
+  (add-hook 'after-init-hook #'post-init-restore))
+
+(setq file-name-handler-alist nil)
+
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
