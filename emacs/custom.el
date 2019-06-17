397dbb8de88a5105573fe58414df6f909ef89ef0
Matt Nakama <matt+git@mattnakama.com>
2019-06-17 08:18:20 -0400

    update emacs customization

--- emacs/custom.el
+++ emacs/custom.el
@@ -8,49 +8,42 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(beacon-color "#ff9da4")
- '(compilation-message-face (quote default))
+ '(compilation-message-face 'default)
  '(cua-global-mark-cursor-color "#2aa198")
  '(cua-normal-cursor-color "#839496")
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (dracula)))
+ '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   (quote
-	("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "2540689fd0bc5d74c4682764ff6c94057ba8061a98be5dd21116bf7bf301acfb" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "36c86cb6c648b9a15d849026c90bd6a4ae76e4d482f7bcd138dedd4707ff26a5" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
+   '("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "2540689fd0bc5d74c4682764ff6c94057ba8061a98be5dd21116bf7bf301acfb" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "36c86cb6c648b9a15d849026c90bd6a4ae76e4d482f7bcd138dedd4707ff26a5" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default))
  '(fci-rule-color "#003f8e")
- '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
- '(frame-background-mode (quote dark))
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
  '(highlight-symbol-colors
    (--map
 	(solarized-color-blend it "#002b36" 0.25)
-	(quote
-	 ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+	'("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2")))
  '(highlight-symbol-foreground-color "#93a1a1")
  '(highlight-tail-colors
-   (quote
-	(("#073642" . 0)
+   '(("#073642" . 0)
 	 ("#546E00" . 20)
 	 ("#00736F" . 30)
 	 ("#00629D" . 50)
 	 ("#7B6000" . 60)
 	 ("#8B2C02" . 70)
 	 ("#93115C" . 85)
-	 ("#073642" . 100))))
+	 ("#073642" . 100)))
  '(hl-bg-colors
-   (quote
-	("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+   '("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00"))
  '(hl-fg-colors
-   (quote
-	("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
+   '("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36"))
+ '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
  '(magit-diff-use-overlays nil)
  '(nrepl-message-colors
-   (quote
-	("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+   '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
  '(package-selected-packages
-   (quote
-	(forge magit-gh-pulls magithub git-link go-snippets yasnippet swiper dracula-theme afternoon-theme solarized-theme challenger-deep-theme ivy-posframe nlinum use-package telephone-line js2-mode smart-tabs-mode evil-collection evil smex avy swiper-helm counsel ivy color-theme-sanityinc-tomorrow go-mode evil-org evil-magit magit evil-visual-mark-mode)))
+   '(go-rename indium kubernetes forge magit-gh-pulls magithub git-link go-snippets yasnippet swiper dracula-theme afternoon-theme solarized-theme challenger-deep-theme ivy-posframe nlinum use-package telephone-line js2-mode smart-tabs-mode evil-collection evil smex avy swiper-helm counsel ivy color-theme-sanityinc-tomorrow go-mode evil-org evil-magit magit evil-visual-mark-mode))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
@@ -59,8 +52,7 @@
  '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
-   (quote
-	((20 . "#ff9da4")
+   '((20 . "#ff9da4")
 	 (40 . "#ffc58f")
 	 (60 . "#ffeead")
 	 (80 . "#d1f1a9")
@@ -77,11 +69,10 @@
 	 (300 . "#ff9da4")
 	 (320 . "#ffc58f")
 	 (340 . "#ffeead")
-	 (360 . "#d1f1a9"))))
+	 (360 . "#d1f1a9")))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-   (quote
-	(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+   '(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))
  '(xterm-color-names
    ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
  '(xterm-color-names-bright
