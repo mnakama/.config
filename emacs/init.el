e887ddfeaa6057a6fc503810fa8c4fe29a9a2e5b
Matt Nakama <matt+git@mattnakama.com>
2024-12-15 18:37:32 -0500

    fix init.el syntax error

--- emacs/init.el
+++ emacs/init.el
@@ -347,18 +347,18 @@
   :config
   (defvar ivy-display-function #'ivy-posframe-display-at-frame-bottom-left))
 
-(if (string-match "nixos" (system-name))
+(if (string-match "nixos" (system-name)) (
 	; enable copilot on the work machine
 	; https://robert.kra.hn/posts/2023-02-22-copilot-emacs-setup/
 	(use-package copilot
-	:load-path (lambda () (expand-file-name "copilot.el" user-emacs-directory))
+	:load-path (lambda () (expand-file-name "copilot.el" user-emacs-directory)))
 	;; don't show in mode line
 	;:diminish
 
 	(use-package org-jira
 	  :config
 	  (setq jiralib-url "https://skytap.atlassian.net"))
-)
+))
 
 ;(global-set-key "\M-x" 'execute-extended-command)
 
