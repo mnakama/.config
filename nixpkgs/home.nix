cebddd27aea48103438724abad93634f6356148e
Matt Nakama <matt+git@mattnakama.com>
2025-12-31 12:59:56 -0500

    update unifi alias
    "stable" doesn't exist anymore. "v9" seems to work fine.
    stable

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -143,7 +143,7 @@ in {
       abcde=''abcde -P -c ${config.xdg.configHome}/abcde.conf'';
       wnas=''wol d0:50:99:7d:59:c0'';
 
-      unifi=''docker run --rm --init -p 8880:8880 -p 8080:8080 -p 8443:8443 -p 8843:8843 -p 3478:3478/udp -p 10001:10001/udp -e TZ='America/New_York' -v ~/unifi:/unifi --name unifi jacobalberty/unifi:stable'';
+      unifi=''docker run --rm --init -p 8880:8880 -p 8080:8080 -p 8443:8443 -p 8843:8843 -p 3478:3478/udp -p 10001:10001/udp -e TZ='America/New_York' -v ~/unifi:/unifi --name unifi jacobalberty/unifi:v9'';
 
       rvnc=''vncviewer -encodings "copyrect tight hextile zlib corre rre raw" -x11cursor -compresslevel 9'';
       xpraattach=''xpra attach tcp:localhost:5000 --encoding png --no-microphone --no-speaker --no-pulseaudio &'';
